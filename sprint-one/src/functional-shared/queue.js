573d0acaad10c99128118499165410567002fcd1
Nick Balestra <nick@balestra.ch>
2015-07-01 17:35:22 -0700

    Added solution to the functional-shared Queue class

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,34 @@
 var Queue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+
+  someInstance.storage = {};
+  someInstance.front = 0;
+  someInstance.back = 0;
+  _.extend(someInstance, queueMethods);
+
+  return someInstance;
 };
 
-var queueMethods = {};
+// To check why Queue.queueMethods brake the tests
+var queueMethods = {
+  enqueue:  function(value){
+              this.storage[this.back++] = value;
+            },
+  dequeue:  function(){
+              if (this.back > this.front) {
+                var value = this.storage[this.front];
+                delete this.storage[this.front++];
+                return value;
+              }
+            },
+  size:     function(){
+              return this.back - this.front;
+            }
+};
+
+
 
 
 
