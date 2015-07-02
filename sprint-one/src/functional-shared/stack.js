4b7a1d6a8dc50814f125c4a36a2540cdbecf9a69
Nick Balestra <nick@balestra.ch>
2015-07-01 17:34:30 -0700

    commented on some weird bug firing up in tests, @spidy is on it

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,30 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = {};
+
+  someInstance.storage = {};
+  someInstance.length = 0;
+  _.extend(someInstance, stackMethods);
+
+  return someInstance;
 };
 
-var stackMethods = {};
+// To check why Stack.stackMethods brake the tests
+var stackMethods = {
+  size: function(){
+          return this.length;
+        },
+  pop:  function(){
+          if (this.length > 0) {
+            var value = this.storage[--this.length];
+            delete this.storage[this.length];
+            return value;
+          }
+        },
+  push: function(value){
+          this.storage[this.length++] = value;
+        }
+};
 
 
