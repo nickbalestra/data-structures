23fcadb8ee7e04f2f651516a1855abde2db72115
Nick Balestra <nick@balestra.ch>
2015-10-08 14:20:07 -0700

    functional-shared classes cleanup

--- stacks-queues/src/functional-shared/queue.js
+++ stacks-queues/src/functional-shared/queue.js
@@ -1,37 +1,28 @@
-var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+var Queue = function(){
   var someInstance = {};
-
   someInstance._storage = {};
   someInstance._front = 0;
   someInstance._back = 0;
   _.extend(someInstance, queueMethods);
-
   return someInstance;
 };
 
-var queueMethods = {};
 
-queueMethods.enqueue = function(value) {
-  this._storage[this._back++] = value;
-};
+var queueMethods = {
+  
+  enqueue: function(value) {
+    this._storage[this._back++] = value;
+  },
 
-queueMethods.dequeue = function() {
-  if (this.size()) {
-    var value = this._storage[this._front];
-    delete this._storage[this._front++];
-    return value;
+  dequeue: function() {
+    if (this.size()) {
+      var value = this._storage[this._front];
+      delete this._storage[this._front++];
+      return value;
+    }
+  },
+
+  size: function() {
+    return this._back - this._front;
   }
 };
-
-queueMethods.size = function() {
-  return this._back - this._front;
-};
-
-
-
-
-
-
-
--- stacks-queues/src/functional-shared/stack.js
+++ stacks-queues/src/functional-shared/stack.js
@@ -1,31 +1,26 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
   var someInstance = {};
-
   someInstance._storage = {};
   someInstance._length = 0;
   _.extend(someInstance, stackMethods);
-
   return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  
+  pop: function() {
+    if (this.size()) {
+      var value = this._storage[this._length - 1];
+      delete this._storage[this._length--];
+      return value;
+    }
+  },
 
-stackMethods.size = function(){
-  return this._length;
-};
+  push: function(value) {
+    this._storage[this._length++] = value; 
+  },
 
-stackMethods.pop = function(){
-  if (this.size()) {
-    var value = this._storage[--this._length];
-    delete this._storage[this._length];
-    return value;
+  size: function() {
+    return this._length;
   }
 };
-
-stackMethods.push = function(value){
-  this._storage[this._length++] = value;
-}
-
-
