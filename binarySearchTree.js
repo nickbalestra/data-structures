73d1d3652b1a909c4a24c3198325f0c78397e7c5
Nick Balestra <nick@balestra.ch>
2015-10-10 23:02:11 -0700

    Implemented both a breadthFirst and depthFirst methods to walk the BSTOC

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -10,21 +10,21 @@ var BinarySearchTree = function(value){
 
 // Time complexity: O(log(n))
 BinarySearchTree.prototype.insert = function(value) {
-  if (value > this._value) {
-    if (!this._right) {
-      this._right = BinarySearchTree(value);
-      return true;
-    } else {
-      return this._right.insert(value);
-    }
-  }
-
   if (value < this._value) {
     if (!this._left) {
       this._left = BinarySearchTree(value);
       return true;
     } else {
       return this._left.insert(value);
+    }
+  }
+
+  if (value > this._value) {
+    if (!this._right) {
+      this._right = BinarySearchTree(value);
+      return true;
+    } else {
+      return this._right.insert(value);
     }
   }
 };
@@ -35,26 +35,56 @@ BinarySearchTree.prototype.contains = function(target) {
     return true;
   }
 
-  if (this._right && target > this._value) {
-    return this._right.contains(target);
-  } 
-
   if (this._left && target < this._value) {
     return this._left.contains(target);
   }
+
+  if (this._right && target > this._value) {
+    return this._right.contains(target);
+  } 
   
   return false;
 };
 
 // Time complexity: O(n)
 BinarySearchTree.prototype.depthFirstLog = function(cb) {
-  if (this._value) {
-    cb(this._value);
+  // Using a stack to walk the tree in depth first way 
+  // Could be done with arrays and native methods in js, but as we have the
+  // class defined in `stacks-queues/src/prototypal/stack.js`, let's use it
+  var container = Stack(); 
+  var element;
+
+  container.push(this);
+  while (container.size() > 0) {  
+    element = container.pop();
+
+    if (element._right) {
+      container.push(element._right);
+    }
+    if (element._left) {
+      container.push(element._left);
+    }
+    cb(element._value);
   }
-  if (this._right) {
-    this._right.depthFirstLog(cb);
-  }
-  if (this._left) {
-    this._left.depthFirstLog(cb);
+};
+
+// Time complexity: O(n)
+BinarySearchTree.prototype.breadthFirstLog = function(cb) {
+  // / Using a queue to walk the tree in breadth first way 
+  // Could be done with arrays and native methods in js, but as we have the
+  // class defined in `stacks-queues/src/prototypal/queue.js`, let's use it
+  var container = Queue(); 
+  var element;
+
+  container.enqueue(this);
+  while (container.size() > 0) {  
+    element = container.dequeue();
+    if (element._left) {
+      container.enqueue(element._left);
+    }
+    if (element._right) {
+      container.enqueue(element._right);
+    }
+    cb(element._value);
   }
 };
--- spec/binarySearchTreeSpec.js
+++ spec/binarySearchTreeSpec.js
@@ -38,8 +38,46 @@ describe('binarySearchTree', function() {
     var func = function(value){ array.push(value); };
     binarySearchTree.insert(2);
     binarySearchTree.insert(3);
+    expect(binarySearchTree._left._value).to.eql(2);
+    expect(binarySearchTree._right).to.eql(null);
+    binarySearchTree.insert(6);
+    binarySearchTree.insert(7);
+    binarySearchTree.insert(1);
+    binarySearchTree.insert(4);
     binarySearchTree.depthFirstLog(func);
-    console.log(array);
-    expect(array).to.eql([5,2,3]);
+    // BST should look something like this
+    //
+    //          5
+    //       2  -  6
+    //     1 - 3   - 7
+    //         - 4
+    //
+    // If we walk the tree in depth first way we should expect something like [ 5, 2, 1, 3, 4, 6, 7 ]
+    expect(array).to.eql([ 5, 2, 1, 3, 4, 6, 7 ]);
   });
+
+  it('should execute a callback on every value in a tree using "breadthFirstLog"', function(){
+    var array = [];
+    var func = function(value){ array.push(value); };
+    binarySearchTree.insert(2);
+    binarySearchTree.insert(3);
+    expect(binarySearchTree._left._value).to.eql(2);
+    expect(binarySearchTree._right).to.eql(null);
+    binarySearchTree.insert(6);
+    binarySearchTree.insert(7);
+    binarySearchTree.insert(1);
+    binarySearchTree.insert(4);
+    binarySearchTree.breadthFirstLog(func);
+    // BST should look something like this
+    //
+    //          5
+    //       2  -  6
+    //     1 - 3   - 7
+    //         - 4
+    //
+    // If we walk the tree in depth first way we should expect something like [ 5, 2, 6, 1, 3, 7, 4 ]
+    expect(array).to.eql([ 5, 2, 6, 1, 3, 7, 4 ]);
+  });
+
+
 });
